92c0a8939d26a812667ba7c74adf578953d19e02
paulavv1 <165815874+paulavv1@users.noreply.github.com>
2024-06-04 11:01:09 -0500

    Update index.js

--- Tareas-clase/Segundo momento/2-3-callback/index.js
+++ Tareas-clase/Segundo momento/2-3-callback/index.js
@@ -22,16 +22,34 @@ let hideField = () => alert ("Campo Oculto");
 
 validateField (false, showField, hideField)// --> Callback funncion
 
-//ejerciocio
+//ejerciocio calculasdora
 
-function calc(op, fnSum , fnRest) {
-    let number1 = Number (prompt("ingrese el primer numero: "));
-    let number2 = Number(prompt("ingrese el segundo numero: "));
-    if
-    (op === "suma"){
-    fnsum (number1, number2) ;
-    }else{
-        alert (fnRest(number1, number2));
-    }
+'use strict';
+
+// Crear la funcion sumar;
+let suma=function(a,b){
+    suma= a + b;
+    alert("El resultado es" + suma) 
+}
+//funcion resta 
+let resta = (a, b) => a - b; 
+
+//Funcion Calc
+
+function calc(op,fnSum , fnRest) {
+let number1 = Number(prompt("ingrese el primer numero: "));
+let number2 = Number(prompt("ingrese el segundo numero: "));
+if (op === "suma"){
+    fnSum(number1, number2);
+}else{
+
+    alert (fnRest(number1, number2));
+}
+  
+}
+
+//Llamado funcion
+
+calc ("suma",suma,  resta);
 
 //s
